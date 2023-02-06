ac63f4de055bdaee688575298c99a139ab8f4912
Charles Duvigneau <83455242+DuvCharles@users.noreply.github.com>
2023-02-06 11:16:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const request = require("request-promise-native");
+
+try {
+  const apiKey = process.env["TRELLO_API_KEY"];
+  const apiToken = process.env["TRELLO_API_TOKEN"];
+
+  moveCardWhenPullRequestClose(apiKey, apiToken);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function moveCardWhenPullRequestClose(apiKey, apiToken) {
+  const exitLists = JSON.parse(process.env.TRELLO_DEPARTURE_LISTS_ID);
+  const destinationListId = process.env["TRELLO_DESTINATION_LIST_ID"];
+  const start = async () => {
+    const branchName = "feature/branch-name-ky-232";
+    const match = branchName.match(/-ky-(?<cardId>[0-9]*)/);
+    const cardId = match.groups.cardId;
+
+    let cards = [];
+
+    for (item of exitLists) {
+      const newCards = await getCardsOfList(apiKey, apiToken, item);
+      cards = [...cards, ...newCards];
+    }
+
+    cards
+      .filter((card) => cardId === card.idShort.toString())
+      .forEach((card) => {
+        console.log(card);
+        putCard(apiKey, apiToken, card.id, destinationListId);
+      });
+  };
+  start();
+}
+
+function getCardsOfList(apiKey, apiToken, listId) {
+  return new Promise(function (resolve, reject) {
+    request(
+      `https://api.trello.com/1/lists/${listId}/cards?key=${apiKey}&token=${apiToken}`
+    )
+      .then(function (body) {
+        resolve(JSON.parse(body));
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+function putCard(apiKey, apiToken, cardId, destinationListId) {
+  const options = {
+    method: "PUT",
+    url: `https://api.trello.com/1/cards/${cardId}?key=${apiKey}&token=${apiToken}`,
+    form: {
+      idList: destinationListId,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    request(options)
+      .then(function (body) {
+        resolve(JSON.parse(body));
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
